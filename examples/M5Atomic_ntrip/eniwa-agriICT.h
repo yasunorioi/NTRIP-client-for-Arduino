e177e627fae573986e096cf8b3d06fe66c78d890
yasunorioi <bb.folf@gmail.com>
2023-03-24 22:28:46 +0900

    add eniwa-agriICT.h

---
+++ examples/M5Atomic_ntrip/eniwa-agriICT.h
@@ -0,0 +1,39 @@
+#ifndef ENIWA_AGRIICT
+#define ENIWA_AGRIICT
+
+uint8_t baseCount=2;
+char* host[]={
+  //Red
+  "rtk.toiso.fit",
+  //Green
+//  "rtk.toiso.fit",
+  //Blue
+  "117.102.192.33",
+  //light blue
+//  "rtk.toiso.fit"
+};
+int httpPort[]={
+  2101,
+  2101
+//  2101,
+//  2101
+};
+char* mntpnt[]={
+  "eniwa-bd982",
+//  "eniwa-f9p"
+//  "eniwa-kazui",
+  "eniwa-bd970"
+};
+char* user[]={
+  "",
+  ""
+//  "",
+//  ""
+};
+char* passwd[]={
+  "",
+  ""
+//  "",
+//  ""
+};
+#endif
