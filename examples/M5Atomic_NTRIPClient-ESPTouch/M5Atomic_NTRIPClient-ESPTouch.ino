03da4b43464d32ed6272884800f56d8483e36a16
yasu <bb.folf@gmail.com>
2024-04-08 20:54:57 +0900

    add example ESPTouch

---
+++ examples/M5Atomic_NTRIPClient-ESPTouch/M5Atomic_NTRIPClient-ESPTouch.ino
@@ -0,0 +1,114 @@
+/*
+ *  NTRIP client for Arduino Ver. 1.0.0 
+ *  NTRIPClient Sample
+ *  Request Source Table (Source Table is basestation list in NTRIP Caster)
+ *  Request Reference Data 
+ * 
+ * 
+ */
+#include "M5Atom.h"
+#include <WiFi.h>           //Need for ESP32 
+#include "NTRIPClient.h"
+
+
+
+char* host = "rtk.toiso.fit";
+int httpPort = 2101; //port 2101 is default port of NTRIP caster
+char* mntpnt = "eniwa-bd982";
+char* user   = "";
+char* passwd = "";
+NTRIPClient ntrip_c;
+
+void setup() {
+  M5.begin(true, false, true);
+  //Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, 22, 19);
+  delay(10);
+  WiFi.begin();
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    // 10秒以上接続できなかったら抜ける
+    if ( 10000 < millis() ) {
+      break;
+    }
+  }
+  Serial.println("");
+  // 未接続の場合にはSmartConfig待受
+  if ( WiFi.status() != WL_CONNECTED ) {
+    WiFi.mode(WIFI_STA);
+    WiFi.beginSmartConfig();
+    Serial.println("Waiting for SmartConfig");
+    while (!WiFi.smartConfigDone()) {
+      delay(500);
+      Serial.print("#");
+      // 30秒以上接続できなかったら抜ける
+      if ( 30000 < millis() ) {
+        Serial.println("");
+        Serial.println("Reset");
+        ESP.restart();
+      }
+    }
+    // Wi-fi接続
+    Serial.println("");
+    Serial.println("Waiting for WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      // 60秒以上接続できなかったら抜ける
+      if ( 60000 < millis() ) {
+        Serial.println("");
+        Serial.println("Reset");
+        ESP.restart();
+      }
+    }
+    Serial.println("");
+    Serial.println("WiFi Connected.");
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println("Requesting SourceTable.");
+  if(ntrip_c.reqSrcTbl(host,httpPort)){
+    char buffer[512];
+    delay(5);
+    while(ntrip_c.available()){
+      ntrip_c.readLine(buffer,sizeof(buffer));
+      Serial.print(buffer); 
+    }
+  }
+  else{
+    Serial.println("SourceTable request error");
+  }
+  Serial.print("Requesting SourceTable is OK\n");
+  ntrip_c.stop(); //Need to call "stop" function for next request.
+  
+  Serial.println("Requesting MountPoint's Raw data");
+  if(!ntrip_c.reqRaw(host,httpPort,mntpnt,user,passwd)){
+    delay(15000);
+    ESP.restart();
+  }
+  Serial.println("Requesting MountPoint is OK");
+  configTime(9 * 3600L, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
+  
+}
+struct tm timeInfo;
+char s[20];
+int count;
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(ntrip_c.available()) {
+        char ch = ntrip_c.read();        
+        Serial2.print(ch);
+        Serial2.flush();
+        count++;
+  }
+  getLocalTime(&timeInfo);
+  sprintf(s, "%04d/%02d/%02d %02d:%02d:%02d",
+          timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday,
+          timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+  Serial.print(s);
+  Serial.print(" count:");
+  Serial.println(count);
+  delay(1000);
+}
