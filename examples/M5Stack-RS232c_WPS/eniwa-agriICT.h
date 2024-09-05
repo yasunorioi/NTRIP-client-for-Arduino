ea440e1f89bb737fe991b6fc3332ba5f46cd6ee7
yasunorioi <bb.folf@gmail.com>
2024-09-05 18:15:50 +0900

    add server mountpoint

--- examples/M5Stack-RS232c_WPS/eniwa-agriICT.h
+++ examples/M5Stack-RS232c_WPS/eniwa-agriICT.h
@@ -1,40 +1,43 @@
 #ifndef ENIWA_AGRIICT
 #define ENIWA_AGRIICT
 
-uint8_t baseCount=2;
+uint8_t baseCount=4;
 char* host[]={
   //Red
   "rtk.toiso.fit",
   //Green
   "rtk.toiso.fit",
   //Blue
-  "117.102.192.33"
+//  "117.102.192.33"
   //light blue
-//  "rtk.toiso.fit"
+  "rtk.toiso.fit",
+  "rtk.toiso.fit"
 };
 int httpPort[]={
   2101,
   2101,
+  2101,
   2101
-//  2101
 };
 char* mntpnt[]={
   "eniwa-bd982",
-    "eniwa-bd970",
+  "eniwa-bd970",
+  "eniwa-bd98223",
+  "enwia-bd982-3"
 //  "eniwa-f9p"
- "eniwa-kazui"
+// "eniwa-kazui"
 
 };
 char* user[]={
   "",
   "",
-//  "",
- ""
+  "",
+  "",
 };
 char* passwd[]={
   "",
   "",
-//  "",
+  "",
   ""
 };
 #endif
